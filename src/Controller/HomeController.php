d222c91992e8d971e675ad7ec53618cbb8578737
EloDev21 <pmtraore@gmail.com>
2022-01-22 15:45:47 +0100

    cgu cgv mentions legales

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,4 +17,22 @@ class HomeController extends AbstractController
         // throw $this->createNotFoundException('Page perdue ou inexistante!');
         return $this->render('home/index.html.twig');
     }
+    /**
+     * @Route("/mentions_legales", name="mentions_legales")
+     */
+    public function mentions(): Response
+    {
+        // on génére une exception
+        // throw $this->createNotFoundException('Page perdue ou inexistante!');
+        return $this->render('mentions/index.html.twig');
+    }
+    /**
+     * @Route("/conditions_d_utilisation", name="conditions_d_utilisation")
+     */
+    public function cgu(): Response
+    {
+        // on génére une exception
+        // throw $this->createNotFoundException('Page perdue ou inexistante!');
+        return $this->render('mentions/cgucgv.html.twig');
+    }
 }
